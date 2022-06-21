1c37f8023a5c402e80315471e0100b60b59df7c6
rust <vavruska.pavel@gmail.com>
2022-06-21 20:14:05 +0200

    feat: Handling post request /add for adding new tasks into DB

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,21 @@ use std::env;
 use std::sync::Mutex;
 use sqlx;
 use actix_web::web::Data;
-use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+use serde::Deserialize;
 
 
+/// Task  
 /// CREATE TABLE public.task (
 ///	description varchar NOT NULL,
 ///	priority int NOT NULL,
 ///	id int NOT NULL GENERATED ALWAYS AS IDENTITY
 /// );
 /// 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 struct Task {
     description: String,
-}
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
 }
 
 async fn list_view(data: Data<Mutex<MyPool>>) -> impl Responder {
@@ -39,9 +32,33 @@ async fn list_view(data: Data<Mutex<MyPool>>) -> impl Responder {
     )
     .fetch_all(&my_pool.pool) // -> Vec<Task>
     .await.unwrap();
+
+    let output: String = tasks.iter().map(|x| x.description.to_owned() + ", ").collect();
    
-    HttpResponse::Ok().body(format!("Hey there! {:?}", tasks))
+    HttpResponse::Ok().body(format!("Tasks to do: {}", output))
 }
+
+
+// This handler is only called if:
+// - request headers declare the content type as `application/x-www-form-urlencoded`
+// - request payload is deserialized into a `Task` struct from the URL encoded format
+async fn add_task_view(form: web::Form<Task>, data: Data<Mutex<MyPool>>) -> impl Responder {
+    println!("Add task view: {}", &form.description);
+    let my_pool = &data.lock().unwrap();
+    let rec = sqlx::query!(
+        r#"
+INSERT INTO public.task ( description, priority )
+VALUES ( $1, 1 )
+RETURNING id
+        "#,
+        &form.description,
+    )
+    .fetch_one(&my_pool.pool)
+    .await.unwrap();
+
+    HttpResponse::Ok().body(format!("Task added: {}", rec.id))
+}
+
 
 struct MyPool {
      pool: sqlx::Pool<sqlx::Postgres>,
@@ -64,8 +81,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(Data::clone(&data))
             .route("/", web::get().to(list_view))
-            .service(hello)
-            .service(echo)
+            .route("/add", web::post().to(add_task_view))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
